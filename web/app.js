b679e9dc73803fad434ffd4b66e7537e14493bd6
wujiaen11 <1486792045@qq.com>
2026-03-21 15:18:54 +0800

    Update app.js

--- web/app.js
+++ web/app.js
@@ -17,7 +17,7 @@ const API_BASE_URL = (typeof process !== 'undefined' && process.env && process.e
     ? process.env.API_BASE_URL
     : (window.ENV && window.ENV.API_BASE_URL)
         ? window.ENV.API_BASE_URL
-        : 'https://xianguo.site/api';
+        : 'https://xianguo-217100-7-1320842230.sh.run.tcloudbase.com/api';
 
 // 模拟管理员账号
 const ADMIN_CREDENTIALS = {
